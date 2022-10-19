87c60df2276a82a201c9eec5d97c586500194212
FreerGit <fredrik-98@live.se>
2022-10-19 20:33:58 +0200

    merge two sorted lists recursive

--- src/lc_0021_merge_two_sorted_lists.rs
+++ src/lc_0021_merge_two_sorted_lists.rs
@@ -21,44 +21,28 @@ pub fn from_vec(vec: &[i32]) -> Option<Box<ListNode>> {
     }
     result
 }
+
 pub fn merge_two_lists(
     list1: Option<Box<ListNode>>,
     list2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
-    // let mut list = vec![];
-    let mut eles_left = true;
-    let mut l1_iter = list1.clone();
-    let mut l2_iter = list2.clone();
-    let mut answer = vec![];
-    while eles_left {
-        println!("fdsfsdfs");
-        match [l1_iter.clone(), l2_iter.clone()] {
-            [Some(l1), Some(l2)] => {
-                if l1.val < l2.val {
-                    answer.push(l1.val);
-                    l1_iter = l1.clone().next;
-                } else if l1.val == l2.val {
-                    answer.push(l1.val);
-                    answer.push(l2.val);
-                    l1_iter = l1.clone().next;
-                    l2_iter = l2.clone().next;
-                } else {
-                    answer.push(l2.val);
-                    l2_iter = l2.clone().next;
-                }
+    match (list1, list2) {
+        (None, None) => None,
+        (Some(n), None) | (None, Some(n)) => Some(n),
+        (Some(l1), Some(l2)) => {
+            if l1.val >= l2.val {
+                Some(Box::new(ListNode {
+                    val: l2.val,
+                    next: merge_two_lists(Some(l1), l2.next),
+                }))
+            } else {
+                Some(Box::new(ListNode {
+                    val: l1.val,
+                    next: merge_two_lists(l1.next, Some(l2)),
+                }))
             }
-            [Some(l1), None] => {
-                answer.push(l1.val);
-                l1_iter = l1.clone().next;
-            }
-            [None, Some(l2)] => {
-                answer.push(l2.val);
-                l2_iter = l2.clone().next;
-            }
-            _ => eles_left = false,
         }
     }
-    return from_vec(&answer);
 }
 
 #[cfg(test)]
